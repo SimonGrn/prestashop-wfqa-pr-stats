a7c54549ed5118cc71260067c943f5e71b0ba1c6
Simon Garny <simon.garny@prestashop.com>
2020-08-20 19:00:37 +0200

    remove jquery

--- index.php
+++ index.php
@@ -95,7 +95,6 @@ $colors = [
     <hr>
     <canvas id="data"></canvas>
 </div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 <script>
   var config = {
